506595cb69abbfc8815f471e20faf6fb208b1053
LocTran-saritasa <loc.tran@saritasa.com>
2024-02-21 11:06:00 +0700

    Set up import alias

---
+++ projects/admin-app/src/components/testing.tsx
@@ -0,0 +1,4 @@
+import { FC } from "react";
+
+const TestingComponent: FC = () => <h1>Hello</h1>
+export default TestingComponent
---
+++ projects/public-app/app/testing.tsx
@@ -0,0 +1,4 @@
+import { FC } from "react";
+
+const TestingComponent2: FC = () => <h1>Hello</h1>
+export default TestingComponent2
