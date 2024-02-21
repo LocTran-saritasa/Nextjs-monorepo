5e0bc02a3bdf004d5b562c80700b43e53822f698
LocTran-saritasa <loc.tran@saritasa.com>
2024-02-21 11:10:08 +0700

    set up projects structure

--- projects/admin-app/src/components/testing.tsx
+++
@@ -1,4 +0,0 @@
-import { FC } from "react";
-
-const TestingComponent: FC = () => <h1>Hello</h1>
-export default TestingComponent
--- projects/public-app/app/testing.tsx
+++
@@ -1,4 +0,0 @@
-import { FC } from "react";
-
-const TestingComponent2: FC = () => <h1>Hello</h1>
-export default TestingComponent2
